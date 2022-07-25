5bf74eb0e99d91cc812f041802b0c6b239586da9
igor guedes <igor.guedes@estudante.ufjf.br>
2022-07-25 14:57:45 -0300

    organizando seções

--- torneio.js
+++ torneio.js
@@ -1,4 +1,9 @@
+const form = document.getElementById("form");
+const nome = document.getElementById("nome");
+const idade = document.getElementById("idade");
 
-
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
 
 
